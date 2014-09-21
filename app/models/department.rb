06ff7a3aa1616041d69fedba6416e098163442b3
James Kirk <jimeux@gmail.com>
2014-09-21 23:25:03 +0900

    Some fixes to deploy config and header.

--- app/models/department.rb
+++ app/models/department.rb
@@ -4,6 +4,8 @@ class Department < ActiveRecord::Base
   has_many :items, -> { latest }
   has_many :preview_items, -> { latest_in_dept }, class_name: 'Item'
 
+  default_scope -> { order('name ASC') }
+
   def to_param
     "#{id} #{name.downcase}".parameterize
   end
