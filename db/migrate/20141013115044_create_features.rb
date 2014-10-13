b558ef5590f84bebcf6193ef7ea960677848cd23
James Kirk <jimeux@gmail.com>
2014-10-13 22:43:28 +0900

    Removed stray comma.

--- db/migrate/20141013115044_create_features.rb
+++ db/migrate/20141013115044_create_features.rb
@@ -1,7 +1,7 @@
 class CreateFeatures < ActiveRecord::Migration
   def change
     create_table :features do |t|
-      t.references :item, unique: true,
+      t.references :item, unique: true
       t.text       :image
     end
   end
