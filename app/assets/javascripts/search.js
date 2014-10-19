11b612fa34c975f79521d1b87bbddd3f6dedb24f
baynescorps <baynescorps@googlemail.com>
2014-10-19 01:04:51 +0100

    removed unused js var

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,6 +1,5 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-var $searchButton = $($("#search-button")[0]);
 
 var searchGoEvent = function(e)
 {
